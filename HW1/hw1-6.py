41dafd15951c081b1ee216c93ba6b9a563198a0a
chamorcev <chamorcev@gmail.com>
2022-02-06 21:53:41 +0300

    hw1

--- HW1/hw1-6.py
+++ HW1/hw1-6.py
@@ -2,20 +2,17 @@
 Создать текстовый файл test_file.txt, заполнить его тремя строками: «сетевое программирование», «сокет», «декоратор».
 Проверить кодировку файла по умолчанию. Принудительно открыть файл в формате Unicode и вывести его содержимое.
 """
-
 import locale
 
+print('Задание 6')
 info = ['сетевое программирование', 'сокет', 'декоратор']
-
 with open('test_file.txt', 'w+') as f:
     for i in info:
         f.write(i + '\n')
     f.seek(0)
-
 print(f)
 
 file_coding = locale.getpreferredencoding()
-
 with open('test_file.txt', 'r', encoding=file_coding) as f:
     for i in f:
         print(i)
