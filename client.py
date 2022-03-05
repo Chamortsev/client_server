eb0a9a010708fc4a2fa502cde6443fbfcff8e492
chamorcev <chamorcev@gmail.com>
2022-03-05 12:55:28 +0300

    hw6

--- client.py
+++ client.py
@@ -7,13 +7,31 @@ import random
 import sys
 import logging
 import logs.config_client_log
+import inspect
 
 log = logging.getLogger('client')
 
+enable_tracing = True
+
+
+def my_log(func):
+    if enable_tracing:
+        def callfunc(*args, **kwargs):
+            log.info("Функция %s: вызвана из функции  %s" % (func.__name__, inspect.stack()[1][3]))
+            r = func(*args, **kwargs)
+            log.info("%s вернула %s" % (func.__name__, r))
+            return r
+        return callfunc
+    else:
+        return func
+
+
+@my_log
 def send_msg(my_message):
     CLIENT_SOCK.send(create_message(my_message))
 
 
+@my_log
 def get_serv_address():
     try:
         server_address = sys.argv[sys.argv.index('addr') + 1]
@@ -23,7 +41,7 @@ def get_serv_address():
         sys.exit(1)
     return server_address
 
-
+@my_log
 def get_serv_port():
     try:
         server_port = int(sys.argv[sys.argv.index('-p') + 1])
@@ -37,7 +55,7 @@ def get_serv_port():
 
     return server_port
 
-
+@my_log
 def prepare_client():
     server_address = DEFAULT_IP_ADDRESS
     if 'addr' in sys.argv:
